2542df8e62cbf7c9a0ccb15e3a4d9dfbcb006405
Sophus Ingi <Sophussophusson@gmail.com>
2023-11-23 10:54:32 +0100

    filehandler and controller added

--- src/SwimmingClubProgram.java
+++ src/SwimmingClubProgram.java
@@ -20,7 +20,7 @@ public class SwimmingClubProgram {
             System.out.println("What is the member's age?");
             int age = scanner.nextInt();
 
-            System.out.println("What is the member's membershipType?");
+            System.out.println("What is the member's membershipType? (active or passive)");
             String membershipType = scanner.next();
 
             System.out.println("What is the member's CPR number?");
@@ -32,10 +32,10 @@ public class SwimmingClubProgram {
             System.out.println("What is the member's email?");
             String email = scanner.next();
 
-            System.out.println("What is the member's adress?");
-            String adress = scanner.next();
+            System.out.println("What is the member's address?");
+            String address = scanner.next();
 
-            members.add(new ClubMember(name, age, membershipType, cprNumber, phoneNumber, email, adress));
+            members.add(new ClubMember(name, age, membershipType, cprNumber, phoneNumber, email, address));
 
             //calculate dues for the created member
             List<Double> duesList = treasurer.calculateDues(members);
@@ -43,27 +43,15 @@ public class SwimmingClubProgram {
             //display the calculated dues
             for (int i = 0; i < duesList.size(); i++) {
                 System.out.println("Payment for " + members.get(i).getName() + ": DKK " + duesList.get(i));
-                System.out.println("Additional Info: " + members.get(i).getCprNumber()); //TODO add all the info to printline
+                System.out.println("Additional Info: ");
+                System.out.println("CprNumber " + members.get(i).getCprNumber()); //TODO add all the info to println
+                System.out.println("Phone number: " + members.get(i).getPhoneNumber());
+                System.out.println("Email address: " + members.get(i).getEmail());
+                System.out.println("Address: " + members.get(i).getAddress());
             }
         }
         //TODO add additional functionality can be implemented and tested here
     }
 }
-/*
-        Member member1 = new Member("Micheal Phelps", 25, "active");
-        Member member2 = new Member("John Lennon", 65, "active");
-        Member member3 = new Member("Forrest Gump", 17, "active");
-        Member member4 = new Member("new guy not active", 20, "passive");
 
-        Treasurer treasurer = new Treasurer();
-        List<Double> duesList = treasurer.calculateDues(member1, member2, member3, member4);
-
-        for (int i = 0; i < duesList.size(); i++) {
-            System.out.println("Payment for " + "Member " + (i + 1) + ": DKK " + duesList.get(i));
-        }
-    }
-
-
-}
-*/
 
